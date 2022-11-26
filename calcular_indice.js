861458ecddbcb6420e3bf2ed3aef64eccd3e23db
Feenri03 <rijoferwis@gmail.com>
2022-11-26 19:57:01 -0400

    Agregando js

---
+++ calcular_indice.js
@@ -0,0 +1,16 @@
+//  function Multiplicar()
+// {
+//     var creditos = Number(document.getElementById('ingresar').Value);
+//     const sumatoria = 520;
+//     var total = creditos * sumatoria
+//     document.getElementById('pago').Value = total;
+// }
+
+function Resultado()
+{
+    var Creditos = Number(document.getElementById('ingresar').value);
+    const Sumatoria = 520;
+    var Total = Creditos * Sumatoria
+    document.getElementById('pago').value = Total;
+}
+
